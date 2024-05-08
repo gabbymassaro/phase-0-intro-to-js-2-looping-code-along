f45f88f9f34f582f4dd68e99b1433d698a3da59f
Gabriella Massaro <gmassaro807@gmail.com>
2024-05-08 14:12:48 -0400

    Refactor previous attempt at looping code along

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Code your solutions in this file
+const peopleToThank = ["Piotr", "Gabby", "Louis"];
+
+function writeCards(peopleToThank) {
+  const thankYouCards = [];
+  for (let i = 0; i < peopleToThank.length; i++) {
+      thankYouCards.push(`Thank you, ${peopleToThank[i]}, for the wonderful surprise gift!`);
+      debugger;
+  }
+  return thankYouCards;
+}
+
+writeCards(peopleToThank);
+
+function countDown(count){
+  while (count >= 0) {
+    console.log(count--);
+  }
+}
